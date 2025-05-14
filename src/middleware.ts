94f5f81e8d8cb5fbcbcc4a57d1e09c5180d59b7d
dejavuu009 <dejavuu009@gmail.com>
2025-05-14 16:29:48 +0200

    JWT swap in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get('auth')?.value
+  const userId = request.cookies.get('userId')?.value
   const { pathname } = request.nextUrl
 
   // Lista stron publicznych (dostęp bez logowania)
@@ -12,16 +12,17 @@ export function middleware(request: NextRequest) {
     return NextResponse.next()
   }
 
-  // Jeśli brak tokena – przekierowanie na login
-  if (!token) {
+  // Jeśli brak userId – przekierowanie na login
+  if (!userId) {
     const loginUrl = request.nextUrl.clone()
     loginUrl.pathname = '/'
     return NextResponse.redirect(loginUrl)
   }
 
-  // Token jest, przepuszczamy dalej
+  // Jest userId – przepuszczamy dalej
   return NextResponse.next()
 }
+
 export const config = {
   matcher: [
     // Wszystkie strony (oprócz API, _next itp.)
