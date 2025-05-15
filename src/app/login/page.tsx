10c741291d3152c115c5c82efd5568d0152433fe
dejavuu009 <dejavuu009@gmail.com>
2025-05-15 10:49:07 +0200

    test czy work

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -10,6 +10,8 @@ export default function LoginPage() {
   const [password, setPassword] = useState('')
   const [error, setError] = useState('')
 
+
+  //test czy work 
   const handleLogin = async () => {
     try {
       const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/login`, {
